86c4ac829742f182f3d172df62fb2870876f60fc
Shadow <145957007+shadow603@users.noreply.github.com>
2024-02-22 00:15:46 +0530

    Update watch.py

--- database/watch.py
+++ database/watch.py
@@ -1,6 +1,6 @@
 from info import DATABASE_URI, DATABASE_NAME
 from motor.motor_asyncio import AsyncIOMotorClient
-
+from fuzzywuzzy import fuzz, process
 client = AsyncIOMotorClient(DATABASE_URI)
 db = client[DATABASE_NAME]
 
