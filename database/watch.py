df6d0d28923be0a00a787e381d121ea2bc4aabe2
Shadow <145957007+shadow603@users.noreply.github.com>
2024-02-22 00:31:33 +0530

    Update watch.py

--- database/watch.py
+++ database/watch.py
@@ -16,7 +16,7 @@ async def search_movie_db(title_query):
     best_score = 0
     async for movie in db.movies.find({}, {"title": 1}):
         movie_title = movie['title']
-        score = fuzz.partial_ratio(title_query, movie_title)
+        score = fuzz.ratio(title_query, movie_title)
         if score > 80 and score > best_score:
             best_match = movie_title
             best_score = score
