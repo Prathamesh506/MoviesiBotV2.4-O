5b63a39616516af7baee60ca5e10d58a9a559374
Shadow <145957007+shadow603@users.noreply.github.com>
2024-04-11 17:57:52 +0530

    Update pm_Filter.py

--- plugins/pm_Filter.py
+++ plugins/pm_Filter.py
@@ -74,7 +74,7 @@ async def auto_filter(client, msg):
     if is_invalid_message(msg) or contains_url(msg.text):
         return
 
-    as_msg = await msg.reply_text("<b>Searching..</b>")
+    # as_msg = await msg.reply_text("<b>Searching..</b>")
 
     #BASED ON PRIVIOUS SEARCH FILTER ADD ON
     if not search_details['title'] and not search_details['year']:
@@ -85,7 +85,7 @@ async def auto_filter(client, msg):
         search = f"{last_search} {search}"
         search_details, search = detail_extraction(search)
         files, offset, total_pages = await search_db(search.lower(), offset=0)
-        await as_msg.delete()
+        # await as_msg.delete()
         if not files:
             await no_resultx(msg, text=f"<i>No Files Found in Database\n<b>For Your Search:</b> {search.title()}</i>")
             return
@@ -96,7 +96,7 @@ async def auto_filter(client, msg):
         files, offset, total_pages = await search_db(search.lower(), offset=0)
         if files:
             await db.store_search(msg.from_user.id, search)
-            await as_msg.delete()
+            # await as_msg.delete()
 
         #LOCAL AUTOCORRECT
         else:
