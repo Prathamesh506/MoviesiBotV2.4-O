8505138d1c627022ff3deec825c42b1892e97629
Shadow <145957007+shadow603@users.noreply.github.com>
2024-07-20 00:31:17 +0530

    Update pm_Filter.py

--- plugins/pm_Filter.py
+++ plugins/pm_Filter.py
@@ -1,30 +1,8 @@
-import re
-import ast
-import math
-import imdb
-import html
-import imdb
-import regex
-import psutil
+
 import asyncio
 import logging
-import pyrogram
-from datetime import datetime,timedelta
-from info import OWNER_USERNAME
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from pyrogram import Client, filters, enums
-from pyrogram.errors import MessageNotModified
-from fuzzywuzzy import fuzz, process
-from Script import script
-from plugins.iwatch import watch_movies_filter
-from utils import get_size, is_subscribed, temp, check_verification, get_token
-from info import ADMINS, AUTH_CHANNEL, NO_RES_CNL,GRP_LINK, CUSTOM_FILE_CAPTION, IS_VERIFY, HOW_TO_VERIFY, DLT,PROTECT_CONTENT,UPIQRPIC
-from database.users_chats_db import db
-from database.watch import store_movies_from_text,does_movie_exxists,search_movie_db
-from database.ia_filterdb import Media, get_file_details,search_db,send_filex
 
-lock = asyncio.Lock()
-ia = imdb.IMDb()
+from pyrogram import Client, filters
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
