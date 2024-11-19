c3c1297f2a0ebd272daf5a5e8804d59299ae374c
Prathamesh <145957007+Prathamesh506@users.noreply.github.com>
2024-11-19 08:11:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,23 +32,23 @@ def is_enabled(value, default):
 # PICS = (environ.get('PICS', 'https://telegra.ph/file/6d0ac3a97740c0d9b4932.jpg https://telegra.ph/file/e2c6fcd43ec729f63572a.jpg https://telegra.ph/file/a6328a8ea1ed1bfbe0e6d.jpg https://telegra.ph/file/e08b02941d5738d6b934b.jpg https://telegra.ph/file/4df4f4f20c19bd00f9908.jpg')).split()
 
 
-# #--------------------------- SCARLET WITCH -----------------------------------
+#--------------------------- SCARLET WITCH -----------------------------------
 
-# BOT_TOKEN = environ.get("BOT_TOKEN", "7212030742:AAE6iH-MyuJaTZneH3awOizG8V8ExXM0mMA")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7212030742:AAE6iH-MyuJaTZneH3awOizG8V8ExXM0mMA")
 
-# DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://scarlet:scarlet@cluster0.iibbctz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://scarlet:scarlet@cluster0.iibbctz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 
-# PICS = (environ.get('PICS', 'https://telegra.ph/file/9203de86a8e55d14fa304.jpg https://telegra.ph/file/b9ed75fcef91d7edd629b.jpg https://telegra.ph/file/931798c653da0fa85b182.jpg https://telegra.ph/file/45162159d9b57fe6b270c.jpg ')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/9203de86a8e55d14fa304.jpg https://telegra.ph/file/b9ed75fcef91d7edd629b.jpg https://telegra.ph/file/931798c653da0fa85b182.jpg https://telegra.ph/file/45162159d9b57fe6b270c.jpg ')).split()
 
 
 #--------------------------- MAKIMA -----------------------------------
 
-BOT_TOKEN = environ.get("BOT_TOKEN", "7489881020:AAG6nLxfH7lVz8rQi7sCIsX7XfhPsW2mLxI")
+# BOT_TOKEN = environ.get("BOT_TOKEN", "7489881020:AAG6nLxfH7lVz8rQi7sCIsX7XfhPsW2mLxI")
 
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://makima:makima@cluster0.gkbgz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+# DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://makima:makima@cluster0.gkbgz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-PICS = (environ.get('PICS', 'https://telegra.ph/file/7ad2ddfeedf7396acb82f.jpg')).split()
+# PICS = (environ.get('PICS', 'https://telegra.ph/file/7ad2ddfeedf7396acb82f.jpg')).split()
 
 
 #--------------------------- TESTING -----------------------------------
